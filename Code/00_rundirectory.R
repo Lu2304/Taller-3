cbb985767c6a0c7069144edeb847886f48f118f9
Luisa Fernanda Vargas Aponte <asiul2304@gmail.com>
2026-05-11 20:31:43 -0500

    Actualizar paquetes

--- Code/00_rundirectory.R
+++ Code/00_rundirectory.R
@@ -10,7 +10,7 @@ rm(list = ls())
 #Cargue de librerías
 library(pacman)
 
-p_load(dplyr, stringr, stringi, osmdata, sf)
+p_load(dplyr, stringr, stringi, osmdata, sf, tidyr, caret, xgboost, purrr)
 
 # Establecimiento de paths
 path_input  <- "Input"
